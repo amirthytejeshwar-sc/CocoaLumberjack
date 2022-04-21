686a5297f845b815dfa85d3a21d6cc410e08e58a
amirthytejeshwar-sc <92975117+amirthytejeshwar-sc@users.noreply.github.com>
2022-04-21 09:04:45 +0530

    Update Package@swift-5.4.swift

--- Package@swift-5.4.swift
+++ Package@swift-5.4.swift
@@ -30,7 +30,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "CocoaLumberjack",
-                exclude: ["Supporting Files"]),
+                exclude: ["Supporting Files"],
+                cSettings: [
+                    .headerSearchPath("."),
+                ]),
         .target(name: "CocoaLumberjackSwiftSupport",
                 dependencies: ["CocoaLumberjack"]),
         .target(name: "CocoaLumberjackSwift",
